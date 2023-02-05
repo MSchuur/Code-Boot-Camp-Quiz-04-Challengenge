47cdb7bc7be533886c2cc892ff0524d067a9161c
MSchuur <mschuur31@gmail.com>
2023-02-05 19:12:14 -0400

    Atlering index, CSS and JS to have first question appear after Starting screen is removed

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,8 @@
+var body = document.querySelector(".viewing");
+var divQuestion = document.createElement("div")
+var olEl = document.createElement("ol");
+var liEl = document.createElement("li");
+
 var start = document.querySelector(".sButton");
 
 var question1 = ["Insert the first Question 1 here?", "First Answer", "Second Answer", "Thrid Answer", "Fourth Answer", 0];
@@ -10,12 +15,23 @@ var questions = [question1, question2, question3, question4, question5, question
 
 // Function to remove the start screen on the click of the Start Quiz Button
 function startQuiz(event) {
-    var startEl = document.getElementById("viewing-area");
     var startElButton = document.getElementById("startButton");
-    startEl.remove();
     startElButton.remove();
     console.log("Pressed");
 }
 
+// Function to add element for the questions
+function quizQuestions() {
+    divQuestion.textContent = questions[0][0];
+    body.appendChild(divQuestion);
+}
+
 // Starts the fucntion for the quiz event listener
-start.addEventListener("click", startQuiz);
+start.addEventListener("click", function(event) {
+    startQuiz();
+    quizQuestions();
+    console.log(divQuestion);;
+});
+
+// article.appendChild(divQuestion);
+console.log(divQuestion);
